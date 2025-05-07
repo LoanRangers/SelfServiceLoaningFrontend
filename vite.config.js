39a09847916af1aaf35ec1ad6c8a9690abb9424f
Topias Kinnunen <topiasxd@gmail.com>
2025-05-07 17:07:08 +0300

    vite example deploy test

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import mkcert from 'vite-plugin-mkcert';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
-  base: '/loanrangers/',
+  base: '/SelfServiceLoaningFrontend/',
 });
