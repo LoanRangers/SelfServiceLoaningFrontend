7232286a240b03b9b5afa9fa5d4c254d9e06b45e
Topias Kinnunen <topiasxd@gmail.com>
2025-05-07 16:02:48 +0300

    Added base to vite config default export

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
-})
+  base: '/loanrangers/',
+});
